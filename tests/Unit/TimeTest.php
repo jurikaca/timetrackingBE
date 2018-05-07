f7cf7f85c178d0b19d08870efe338ad6e239d10a
Juri's Pc <juri.kaca@gmail.com>
2018-05-07 17:16:43 +0200

    Setup of php unit testing

---
+++ tests/Unit/TimeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TimeTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     *
+     * @return void
+     */
+    public function testTime()
+    {
+        $this->assertTrue(true);
+    }
+}
